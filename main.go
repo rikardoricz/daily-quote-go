6f8e304f45836cea195ea1239f5c587ea810e252
Tomasz Swiatek <tomasz.swiatek@globallogic.com>
2025-07-02 13:59:35 +0200

    feat: daily quote generator http server
    using external api https://api.realinspire.live/v1/quotes/random

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Quote struct {
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+type PageData struct {
+	Quote  Quote
+	Error  string
+	HasNew bool
+}
+
+func main() {
+	tmpl, err := template.ParseFiles("./template/index.html")
+	if err != nil {
+		log.Fatal("Error parsing template:", err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := PageData{}
+
+		if r.URL.Query().Get("new") == "true" {
+			data.HasNew = true
+		}
+
+		quote, err := fetchQuote()
+		if err != nil {
+			data.Error = "Failed to fetch quote: " + err.Error()
+		} else {
+			data.Quote = quote
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, "Error rendering template", http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println("Server starting on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func fetchQuote() (Quote, error) {
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	resp, err := client.Get("https://api.realinspire.live/v1/quotes/random")
+	if err != nil {
+		return Quote{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return Quote{}, fmt.Errorf("API returned status code: %d", resp.StatusCode)
+	}
+
+	var quotes []Quote
+	err = json.NewDecoder(resp.Body).Decode(&quotes)
+	if err != nil {
+		return Quote{}, err
+	}
+
+	if len(quotes) == 0 {
+		return Quote{}, fmt.Errorf("no quotes returned from API")
+	}
+
+	return quotes[0], nil
+}
